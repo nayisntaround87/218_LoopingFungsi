bf8ca639a9c0ebec9c7b6ab2154263bdf93b532d
nayisntaround87 <naylasalwahayati@gmail.com>
2024-03-19 15:11:32 +0700

    create looping using do while

---
+++ PertemuanKetiga/do while.cpp
@@ -0,0 +1,20 @@
+include <iostream>
+using namespace std;
+
+int main
+{
+    string pilihan;
+    do
+    {
+        cout << "makan" << endl;
+        cout << "main game" << endl;
+        cout << "ibadah" << endl;
+
+        cout << "apakah anda masih hidup? (yes/no)";
+        cin >> pilihan;
+
+
+    } while (pilihan == "yes");
+
+}
+    
